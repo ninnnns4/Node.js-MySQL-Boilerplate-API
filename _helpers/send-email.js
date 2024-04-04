ed7dd1ddc2d9be22e7ddda768dca45f06b4c3144
ninnnns4 <ninoangelotumabiene4@gmail.com>
2024-04-04 17:12:05 +0800

    Account management (CRUD) routes with role based access control

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -1,9 +1,11 @@
-const nodemailer = require('nodemailer'); const config = require('config.json');
+const nodemailer = require('nodemailer');
 const config = require('config.json');
 
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-    const transporter = nodemailer.createTransport(config.smtpOption);
+    const transporter = nodemailer.createTransport(config.smtpOptions);
     await transporter.sendMail({ from, to, subject, html });
 }
+
+
--- accounts/refresh-token.model.js
+++ accounts/refresh-token.model.js
@@ -4,26 +4,27 @@ module.exports = model;
 
 function model(sequelize) {
     const attributes = {
-        token: {type: DataTypes.STRING},
-        expires: { type: DataTypes.DATE},
-        created: {type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW},
-        revoked: { type: DataTypes.STRING},
-        revokedByIp: {type: DataTypes.STRING},
-        replacedByToken: {type: DataTypes.STRING},
+        token: { type: DataTypes.STRING },
+        expires: { type: DataTypes.DATE },
+        created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+        createdByIp: { type: DataTypes.STRING },
+        revoked: { type: DataTypes.DATE },
+        revokedByIp: { type: DataTypes.STRING },
+        replacedByToken: { type: DataTypes.STRING },
         isExpired: {
             type: DataTypes.VIRTUAL,
             get() { return Date.now() >= this.expires; }
         },
         isActive: {
             type: DataTypes.VIRTUAL,
-            get() {return !this.revoked && !this.isExpired; }
+            get() { return !this.revoked && !this.isExpired; }
         }
     };
-    
+
     const options = {
-        //disable default timestamp field (createdAt and updatedAt)
-        timestamp: false
+        // disable default timestamp fields (createdAt and updatedAt)
+        timestamps: false
     };
-    
+
     return sequelize.define('refreshToken', attributes, options);
 }

93a0a06c3296fbda31e1af5f8d547bd11b937101
ninnnns4 <ninoangelotumabiene4@gmail.com>
2024-04-07 14:19:41 +0800

    final

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -7,5 +7,3 @@ async function sendEmail({ to, subject, html, from = config.emailFrom }) {
     const transporter = nodemailer.createTransport(config.smtpOptions);
     await transporter.sendMail({ from, to, subject, html });
 }
-
-
